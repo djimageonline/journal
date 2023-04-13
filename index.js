547eadeca0d2ed7d9a005024047e523170c42289
Johnny Proano <jproano22@yahoo.com>
2023-04-13 01:22:37 -0400

    added renderBlog function, blogshtml function and displayed blogs, styled the grid adding gap and margin

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-import { myBlogs } from "./data";
+import { myBlogs } from "/data.js";
+
+function blogsHtml() {
+  let blogs = "";
+
+  myBlogs.forEach((blog) => {
+    blogs += `
+      <a href="/">
+        <img class="image" src="${blog.img}" alt="${blog.alt}">
+        <p class="date grid-item">${blog.date}</p>
+        <p class="title grid-item">${blog.title}</p>
+        <p class="ptext grid-item">${blog.text}</p>
+      </a>
+    `;
+  });
+
+  return blogs;
+}
+
+function renderBlogs() {
+  document.getElementById("blogs-grid-container").innerHTML = blogsHtml();
+}
+
+renderBlogs();
