1df94f4c5e7f1f5be996d392bf722e5d6d30717f
Johnny Proano <jproano22@yahoo.com>
2023-04-12 22:41:59 -0400

    added data.js with placeholder data

---
+++ data.js
@@ -0,0 +1,44 @@
+export const myBlogs = [
+  {
+    img: "img/jordan.jpg",
+    alt: "picture of michael jordan on side of building",
+    date: "March 28, 2023",
+    title: "Chicago Bulls forever",
+    text: "Cupcake ipsum dolor sit amet dragée carrot cake brownie. Gummies marzipan shortbread cotton candy tiramisu. Caramels bear claw toffee cake tart cake tootsie roll. Chocolate dragée tart pudding cupcake.",
+  },
+  {
+    img: "img/hollywoodstudios.jpg",
+    alt: "picture of toy story soldiers at hollywood studios",
+    date: "Feb 28, 2023",
+    title: "Hollywood Studios",
+    text: "Cupcake ipsum dolor sit amet dragée carrot cake brownie. Gummies marzipan shortbread cotton candy tiramisu. Caramels bear claw toffee cake tart cake tootsie roll. Chocolate dragée tart pudding cupcake.",
+  },
+  {
+    img: "img/mandolorian.jpg",
+    alt: "picture of the mandolorian",
+    date: "Jan 28, 2023",
+    title: "Mandolorian, the best show?",
+    text: "Cupcake ipsum dolor sit amet dragée carrot cake brownie. Gummies marzipan shortbread cotton candy tiramisu. Caramels bear claw toffee cake tart cake tootsie roll. Chocolate dragée tart pudding cupcake.",
+  },
+  {
+    img: "img/soccer.jpg",
+    alt: "picture of a live soccer event at a stadium",
+    date: "Jan 28, 2023",
+    title: "Real Madrid or Barcelona?",
+    text: "Cupcake ipsum dolor sit amet dragée carrot cake brownie. Gummies marzipan shortbread cotton candy tiramisu. Caramels bear claw toffee cake tart cake tootsie roll. Chocolate dragée tart pudding cupcake.",
+  },
+  {
+    img: "img/turntable.jpg",
+    alt: "picture of dj cd turntables",
+    date: "Dec 28, 2022",
+    title: "The art of the Deejay",
+    text: "Cupcake ipsum dolor sit amet dragée carrot cake brownie. Gummies marzipan shortbread cotton candy tiramisu. Caramels bear claw toffee cake tart cake tootsie roll. Chocolate dragée tart pudding cupcake.",
+  },
+  {
+    img: "img/xbox.jpg",
+    alt: "picture of an xbox controller",
+    date: "Nov 28, 2022",
+    title: "Favorite game on xbox",
+    text: "Cupcake ipsum dolor sit amet dragée carrot cake brownie. Gummies marzipan shortbread cotton candy tiramisu. Caramels bear claw toffee cake tart cake tootsie roll. Chocolate dragée tart pudding cupcake.",
+  },
+];
---
+++ index.js
@@ -0,0 +1,1 @@
+import { myBlogs } from "./data";
