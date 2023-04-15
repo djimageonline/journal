e7917b36effd44aa31f397f007ef17621bcab227
Johnny Proano <jproano22@yahoo.com>
2023-04-14 21:40:40 -0400

    added content to myBlog array[0] will add more content to the rest objects, added the content to blogpagehtml function, will refactor but all working accordignly, added css style to blog page.

--- blog.js
+++ blog.js
@@ -8,6 +8,10 @@ function blogPageHTML() {
       <p class="blog-title">${myBlogs[0].title}</p>
       <p class="blog-text">${myBlogs[0].text}</p>
       <img class="blog-image" src="${myBlogs[0].img}" alt="${myBlogs[0].alt}">
+      <p class="blog-subtitle">${myBlogs[0].subtitle}
+      <p class="blog-paragraph">${myBlogs[0].paragraph}</p>
+      <p class="blog-subtitle-two">${myBlogs[0].subtitleTwo}
+      <p class="blog-paragraph-two">${myBlogs[0].paragraphTwo}</p>
   `;
 
   return blogPageContent;
--- data.js
+++ data.js
@@ -5,7 +5,14 @@ export const myBlogs = [
     date: "April 10, 2023",
     title: "My Journey to Software Development",
     text: "Cupcake ipsum dolor sit amet dragée carrot cake brownie. Gummies marzipan shortbread cotton candy tiramisu. Caramels bear claw toffee cake tart cake tootsie roll. Chocolate dragée tart pudding cupcake.",
+    subtitle: "How I stay committed to learning",
+    paragraph:
+      "I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.  <br><br> While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.",
+    subtitleTwo: "How I got started",
+    paragraphTwo:
+      "I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words. <br><br> That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.",
   },
+
   {
     img: "img/jordan.jpg",
     alt: "picture of michael jordan on side of building",
